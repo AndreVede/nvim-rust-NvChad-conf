1d279540374554b5a2e86909124149d44e79d1ec
Zacharie André Aristide Boisnard <mecazack@gmail.com>
2025-02-10 21:57:36 +0100

    clean: trailing space

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,7 +62,7 @@ return {
   },
 
   {
-    'rcarriga/nvim-dap-ui', 
+    'rcarriga/nvim-dap-ui',
     dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
     config = function()
 			require("dapui").setup()
